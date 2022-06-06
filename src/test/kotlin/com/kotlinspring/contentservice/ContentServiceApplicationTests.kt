46f7b868286b889c17e717a605a646bde4422f87
kljopu <kljopuu@gmail.com>
2022-06-06 23:59:26 +0900

    application main 함수 작성

---
+++ src/test/kotlin/com/kotlinspring/contentservice/ContentServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinspring.contentservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ContentServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
